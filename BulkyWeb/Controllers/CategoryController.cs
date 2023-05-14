aa3d4d66cf8346907c0212a9cb02993780949e33
Dionisie <climdinis@gmail.com>
2023-05-15 00:47:41 +0100

    EditFixed

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -37,6 +37,37 @@ namespace BulkyWeb.Controllers
             }
              return View(obj);
 
+        } 
+       
+        public IActionResult Edit(int? id)
+        {
+            if (id==null || id==0)
+            {
+                return NotFound();
+            }
+            Category categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb==null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString()) 
+            {
+                ModelState.AddModelError("name","The DisplayOrder cannot exactly match the Name");
+            }
+            
+            if (ModelState.IsValid) //Validation from model
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();               //Writes in Db
+                return RedirectToAction("Index");
+            }
+             return View(obj);
+
         }
     }
 }
