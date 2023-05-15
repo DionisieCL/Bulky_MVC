0ed4781184cf6d223dd615b30040f0f1e31761f1
Dionisie <climdinis@gmail.com>
2023-05-15 01:10:34 +0100

    Delete

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -55,19 +55,47 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Edit(Category obj)
         {
-            if (obj.Name == obj.DisplayOrder.ToString()) 
-            {
-                ModelState.AddModelError("name","The DisplayOrder cannot exactly match the Name");
-            }
-            
+          
             if (ModelState.IsValid) //Validation from model
             {
-                _db.Categories.Add(obj);
+                _db.Categories.Update(obj);
                 _db.SaveChanges();               //Writes in Db
                 return RedirectToAction("Index");
             }
              return View(obj);
 
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost,ActionName("Delete")]
+
+     public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if (obj==null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+
+            if (ModelState.IsValid) //Validation from model
+            {
+                _db.Categories.Remove(obj);
+                _db.SaveChanges();               //Writes in Db
+                return RedirectToAction("Index");
+            }
+            return View();
         }
     }
 }
